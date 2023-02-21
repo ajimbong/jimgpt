86172cff57ef318ed9f28e0312110c1238edf9aa
ajimbong <ajimbong713@gmail.com>
2023-02-21 17:40:53 +0100

    more bug fixes

--- src/bot.js
+++ src/bot.js
@@ -8,33 +8,16 @@ const bot = new telegramBot(TELEGRAM_TOKEN, {polling: true});
 bot.on('message', async (message)=>{
     const text = message.text;
     const chatId = message.from.id;
-    const isBot = message.from.is_bot;
 
-    const greetedUsers = new Set();
-
-    if (message === '/start') {
-        // Check if the user is not a bot and has not been greeted before
-        if (!isBot && !greetedUsers.has(chatId)) {
-            // Send a welcome message
-            bot.sendMessage(chatId, 'Welcome human, how can I help?.');
-      
-            // Add the user to the set of greeted users
-            greetedUsers.add(chatId);
-        } else {
-            // Send a reply message
-            bot.sendMessage(chatId, 'Welcome back!');
-        }
-    } else {
-        try {
-            const res = await getPrompt(text);
-            bot.sendMessage(chatId, res);
-        }
-        catch (ex) {
-            console.error(ex);
-        }
+    try {
+        const res = await getPrompt(text);
+        bot.sendMessage(chatId, res);
+    }
+    catch (ex) {
+        console.error('There was an error: ', ex.message);
     }
 
-    console.log(message)
+    console.log(text)
 })
 
 bot.on("polling_error", (error) => {
